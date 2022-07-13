78235ffe37a39d80d92cf495e4ee1130deeda5c4
ZJ <zzha9494@uni.sydney.edu.au>
2022-07-13 22:31:40 +1000

    Q21

---
+++ q21_mergeTwoLists.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+        cur1 = list1
+        cur2 = list2
+
+        while cur1 or cur2:
+            if cur1 and cur2:
+                if cur1.val <= cur2.val:
+                    cur.next = cur1
+                    cur1 = cur1.next
+                else:
+                    cur.next = cur2
+                    cur2 = cur2.next
+            elif cur1:
+                cur.next = cur1
+                cur1 = cur1.next
+            else:
+                cur.next = cur2
+                cur2 = cur2.next
+            cur = cur.next
+        return dummy
